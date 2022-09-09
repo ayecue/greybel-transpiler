a6b03bddeb98442c673d2b06607470b038a8c144
ayecue <soerenwehmeier@googlemail.com>
2022-09-10 01:26:15 +0200

    0.5.7 remove whitespaces in boilerplate

--- src/boilerplates/index.ts
+++ src/boilerplates/index.ts
@@ -1,27 +1,27 @@
 import { ASTChunk, Parser } from 'greybel-core';
 
 export const HEADER_BOILERPLATE: ASTChunk = new Parser(
-  `MODULES = {}
-	EXPORTED = {}
-	__REQUIRE = function(r)
+  `MODULES={}
+	EXPORTED={}
+	__REQUIRE=function(r)
 	if (not MODULES.hasIndex(r)) then
-	exit("Module " + r + " cannot be found...")
+	exit("Module "+r+" cannot be found...")
 	end if
-	module = MODULES[r]
+	module=MODULES[r]
 	return @module(r).exports
 	end function`
 ).parseChunk() as ASTChunk;
 
 export const MODULE_BOILERPLATE: ASTChunk = new Parser(
-  `MODULES["$0"] = function(r)
-	module = {}
+  `MODULES["$0"]=function(r)
+	module={}
 	if (EXPORTED.hasIndex(r)) then
-	module = EXPORTED[r]
+	module=EXPORTED[r]
 	end if
 	if (not module.hasIndex("exports")) then
 	"$1"
 	end if
-	EXPORTED[r] = module
+	EXPORTED[r]=module
 	return EXPORTED[r]
 	end function`
 ).parseChunk() as ASTChunk;
