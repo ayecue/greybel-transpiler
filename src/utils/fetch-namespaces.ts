9fe58c9ebde1af085911a6069e126c03c4f3f9de
ayecue <soerenwehmeier@googlemail.com>
2024-12-04 16:42:13 +0100

    3.3.0 update core

--- src/utils/fetch-namespaces.ts
+++ src/utils/fetch-namespaces.ts
@@ -1,10 +1,37 @@
-import { ASTBaseBlockWithScope, ASTChunk } from 'miniscript-core';
+import {
+  ASTBaseBlockWithScope,
+  ASTChunk,
+  ASTIdentifier,
+  ASTMemberExpression,
+  ASTScopeNamespace,
+  ASTType
+} from 'miniscript-core';
+
+function transformNamespaceToString(
+  namespace: ASTScopeNamespace
+): string | null {
+  if (namespace.type === ASTType.MemberExpression) {
+    const memberExpr = namespace as ASTMemberExpression;
+    if (memberExpr.identifier.type === ASTType.Identifier) {
+      return (memberExpr.identifier as ASTIdentifier).name;
+    }
+    return null;
+  } else if (namespace.type === ASTType.Identifier) {
+    return (namespace as ASTIdentifier).name;
+  }
+  return null;
+}
 
 export function fetchNamespaces(chunk: ASTChunk): string[] {
-  const allNamespaces = new Set<string>(chunk.namespaces);
+  const allNamespaces = new Set<string>(
+    chunk.namespaces.map(transformNamespaceToString).filter((it) => it !== null)
+  );
 
   chunk.scopes.reduce((result: Set<string>, scope: ASTBaseBlockWithScope) => {
-    scope.namespaces.forEach((item: string) => allNamespaces.add(item));
+    const scopeNamespaces = scope.namespaces
+      .map(transformNamespaceToString)
+      .filter((it) => it !== null);
+    scopeNamespaces.forEach((item: string) => allNamespaces.add(item));
     return result;
   }, allNamespaces);
 
